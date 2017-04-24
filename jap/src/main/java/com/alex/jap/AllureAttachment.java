3a62331d91259a5455abe0550b5ca225c70938cf
Alexander Popov <apop@ciklum.com>
2017-04-24 17:45:08 +0300

    done

--- jap/src/main/java/com/alex/jap/AllureAttachment.java
+++
@@ -1,8 +0,0 @@
-package com.alexautomician.core;
-
-/**
- * Created by apop on 3/28/2017.
- */
-public interface AllureAttachment {
-    byte[] publish();
-}
--- jap/src/test/java/TestMethods.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by apop on 3/28/2017.
- */
-public class Test {
-}
